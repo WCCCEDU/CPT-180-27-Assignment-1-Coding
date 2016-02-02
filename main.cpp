2b38408ebbf29e9e1f980fa0212069a9f5bd7865
Samantha Shurie <s_shurie@yahoo.com>
2016-02-02 00:42:23 -0500

    All values now have their own variables. Program now does all calculations before printing

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 int main() {
 
+    // given constants
     const int SHARES_BOUGHT = 1000;
     const double PURCHASE_PRICE = 45.50;
     const int SHARES_SOLD = 1000;
@@ -10,9 +11,15 @@ int main() {
     const double COMMISSION_RATE = .02;
 
     // stock purchase calculations
-    double total_stock_price = SHARES_BOUGHT * PURCHASE_PRICE;
-    double total_commission = total_stock_price * COMMISSION_RATE;
-    double purchase_grand_total = total_stock_price + total_commission;
+    double purchase_total_stock_price = SHARES_BOUGHT * PURCHASE_PRICE;
+    double purchase_total_commission = purchase_total_stock_price * COMMISSION_RATE;
+    double purchase_grand_total = purchase_total_stock_price + purchase_total_commission;
+
+    // stock sale calculations
+    double sale_total_stock_price = SHARES_BOUGHT * SALE_PRICE;
+    double sale_total_commission = sale_total_stock_price * COMMISSION_RATE;
+    double sale_grand_total = sale_total_stock_price - sale_total_commission;
+    double total_profit_loss = sale_grand_total - purchase_grand_total;
 
     // print purchase results
     std::cout << "JOE'S ACME SOFTWARE, INC. STOCK VALUES\n\n"; // just some header text
@@ -20,25 +27,19 @@ int main() {
     std::cout << std::setprecision(2) << std::fixed;
     std::cout << std::setw(25) << std::left << "Quantity Purchased: "  << SHARES_BOUGHT << "\n";
     std::cout << std::setw(25) << "Price Per Stock: " << PURCHASE_PRICE << "\n";
-    std::cout << std::setw(25) << "Total Stock Amount: " << "-" << total_stock_price << "\n";
-    std::cout << std::setw(25) << "Broker Commission: " << "-" << total_commission << "\n";
+    std::cout << std::setw(25) << "Total Stock Amount: " << "-" << purchase_total_stock_price << "\n";
+    std::cout << std::setw(25) << "Broker Commission: " << "-" << purchase_total_commission << "\n";
     std::cout << "____________________________________" << "\n";
     std::cout << std::setw(25) << "Grand Total: " << "-$" << purchase_grand_total << "\n\n\n";
-
-    // stock sale calculations
-    total_stock_price = SHARES_BOUGHT * SALE_PRICE;
-    total_commission = total_stock_price * COMMISSION_RATE;
-    double sale_grand_total = total_stock_price - total_commission;
-    double total_profit_loss = sale_grand_total - purchase_grand_total;
 
     // print sale results
     std::cout << "SELLING\n====================================\n";
     std::cout << std::setw(25) << "Quantity Sold: " << SHARES_SOLD << "\n";
     std::cout << std::setw(25) << "Price Per Stock: " <<  SALE_PRICE << "\n";
-    std::cout << std::setw(25) << "Total Stock Value: " << "+" << total_stock_price << "\n";
-    std::cout << std::setw(25) << "Broker Commission: " << "-" << total_commission << "\n";
-    std::cout << "____________________________________" << "\n";
-    std::cout << std::setw(25) << "Grand Total: " << "+$" << sale_grand_total << "\n\n";
-    std::cout << std::setw(25) <<  "TOTAL PROFIT/LOSS: " << "+$" << total_profit_loss << "\n\nEnd of Program";
+    std::cout << std::setw(25) << "Total Stock Value: " << "+" << purchase_total_stock_price << "\n";
+    std::cout << std::setw(25) << "Broker Commission: " << "-" << sale_total_commission << "\n";
+    std::cout << "____________________________________\n";
+    std::cout << std::setw(25) << "Grand Total: " << "+$" << sale_grand_total << "\n\n\n";
+    std::cout << std::setw(25) <<  "TOTAL PROFIT/LOSS: " << "+$" << total_profit_loss << "\n\nEnd of Program\n";
     return 0;
 }
