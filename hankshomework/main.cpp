18f8bc25c4d8d72cc62f80a50243bedc63395bdd
HankofWisdom <wownoob29@gmail.com>
2016-02-02 08:19:52 -0500

    Changed the Constants to have the const. Also fixed spelling and Spacing in my couts.

--- hankshomework/main.cpp
+++ hankshomework/main.cpp
@@ -12,31 +12,32 @@
  * Constants - SHARE_BOUGHT , SHARE_SOLD , COMMISSION_RATE , PURCHASE_PRICE , SALE_PRICE
 */
 
-int SHARE_BOUGHT = 1000 , SHARE_SOLD = 1000 ;
-double COMMISSION_RATE = 0.02, PURCHASE_PRICE = 45.5, SALE_PRICE = 56.9;
+int const SHARE_BOUGHT = 1000 , SHARE_SOLD = 1000 ;
+double const COMMISSION_RATE = 0.02, PURCHASE_PRICE = 45.5, SALE_PRICE = 56.9;
 int main() {
-    double sale_bought , sale_sold , sale_profit , commssion_paid_buy , commssion_paid_sale;
-    char new_line = '\n';
+    double sale_bought = 0.00, sale_sold = 0.00, sale_profit = 0.00,
+            commission_paid_buy = 0.00, commission_paid_sale = 0.00;
+
 
 
     sale_bought = SHARE_BOUGHT * PURCHASE_PRICE;
-    commssion_paid_buy = sale_bought * COMMISSION_RATE;
+    commission_paid_buy = sale_bought * COMMISSION_RATE;
 
-    std::cout <<"You spent $"<<sale_bought<<" on stocks and paid $"<<commssion_paid_buy<<" to your broker."<<new_line;
+    std::cout << "You spent $" << sale_bought << " on stocks and paid $" << commission_paid_buy << " to your broker." << '\n';
 
     sale_sold = SHARE_SOLD * SALE_PRICE;
-    commssion_paid_sale = sale_sold * COMMISSION_RATE;
+    commission_paid_sale = sale_sold * COMMISSION_RATE;
 
-    std::cout <<"You earned $"<<sale_sold<<" on stocks but your broker took $"<<commssion_paid_sale<<new_line;
+    std::cout << "You earned $" << sale_sold << " on stocks but your broker took $" << commission_paid_sale << '\n';
 
-    sale_bought = sale_bought - commssion_paid_buy;
-    sale_sold = sale_sold - commssion_paid_sale;
+    sale_bought = sale_bought - commission_paid_buy;
+    sale_sold = sale_sold - commission_paid_sale;
     sale_profit = sale_sold- sale_bought ;
 
     if (sale_profit > 1)
-        std::cout <<"Yor profit would be $"<<sale_profit<<new_line;
+        std::cout << "Yor profit would be $" << sale_profit << '\n';
     else
-        std::cout <<"Yor Lose would be $-"<<sale_profit<<new_line;
+        std::cout << "Yor Lose would be $-" << sale_profit << '\n';
 
     return 0;
 }
