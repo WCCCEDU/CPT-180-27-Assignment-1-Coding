8fb49453ccf0fcfcaa56f7716593d442feae2b31
thompsonjos <thompsonjos@my.wccc.edu>
2016-02-03 14:41:14 -0500

    1st version of stock program

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // Declare and initilize variable
+    int shares_bought = 1000;
+    int shares_sold = 1000;
+    double purchase_price = 45.50;
+    double commission_rate = 0.02;
+    double selling_price = 56.90;
+
+
+   // Main math function
+    double purchase_commission = (shares_bought * purchase_price * commission_rate);
+    double selling_commission = (shares_sold * selling_price * commission_rate);
+    double total_purchase = ((shares_bought * purchase_price) + purchase_commission);
+    double total_selling = ((shares_bought * selling_price) - selling_commission);
+    double profit_loss = (total_selling - total_purchase);
+
+    // Output display code
+
+    cout << "Joe bought " << shares_bought << " " << "shares of stock"  << " ";
+    cout << "at a price of" << " " << "$" << purchase_price << endl;
+    cout << "The Commission to his broker for his purchase was: " << "$" << fixed(2)purchase_commission << endl;
+    cout << "Joe then sold " << shares_sold << " " << "shares of stock" << " ";
+    cout << "at a price of" << " " << "$" << selling_price << endl;
+    cout << "The Commission to his broker for his sale was:" << " " << "$" << selling_commission << endl;
+    cout << "Joe's total Profit / Loss was" << " " << "$" << profit_loss;
+    cout << "Not bad for a days work"
+
+    return 0;
+}
