9b8fc81e6632f781571838a3065145954dfa5633
Michael Watkins <watkinsmi@my.wccc.edu>
2016-02-03 18:10:51 -0500

    Reworked coding based on comments

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -2,34 +2,35 @@
 #include <iomanip>
 
 int main() {
-    //Stock Transactions: Michael Watkins 1-31-16
+    //Stock Transactions: Michael Watkins 2-1-16
 
-    //Declarations and Initialization
-    double SHARES_BOUGHT = 1000;
-    double SHARES_SOLD = 1000;
-    double COMMISION_RATE = 0.02;
-    double PURCHASE_PRICE = 45.50;
-    double SALE_PRICE = 56.90;
+    //Declarations and Initialization for constants
+    const double SHARES_BOUGHT = 1000;
+    const double SHARES_SOLD = 1000;
+    const double COMMISSION_RATE = 0.02;
+    const double PURCHASE_PRICE = 45.50;
+    const double SALE_PRICE = 56.90;
 
-    //Sale Calculations
-    double STOCK_BOUGHT = SHARES_BOUGHT * PURCHASE_PRICE;
-    double COMMISION_BUY = COMMISION_RATE * STOCK_BOUGHT;
-    double STOCK_SALE = SHARES_SOLD * SALE_PRICE;
-    double COMMISION_SALE = COMMISION_RATE * STOCK_SALE;
-    double PROFIT = STOCK_SALE - STOCK_BOUGHT - COMMISION_BUY - COMMISION_SALE;
-
-    //Output Display
+    //Output display
     std::cout << "Joe's Stock Exchange" << std::endl;
+
+    //Reworked so declarations and initializations are closer to output
+    //Cleaned up lines of code
+    double stock_purchase_price = SHARES_BOUGHT * PURCHASE_PRICE;
     std::cout << "Stock Purchase Price: $" << std::fixed << std::setprecision(2) << std::setfill('0');
-    std::cout << STOCK_BOUGHT << std::endl;
-    std::cout << "Purchase Commission: $" << std::fixed << std::setprecision(2) << std::setfill('0');
-    std::cout << COMMISION_BUY << std::endl;
-    std::cout << "Stock Sale Price: $" << std::fixed << std::setprecision(2) << std::setfill('0');
-    std::cout << STOCK_SALE << std::endl;
-    std::cout << "Sale Commission: $" << std::fixed << std::setprecision(2) << std::setfill('0');
-    std::cout << COMMISION_SALE << std::endl;
-    std::cout << "Total Profit: $" << std::fixed << std::setprecision(2) << std::setfill('0');
-    std::cout << PROFIT << std::endl;
+    std::cout << stock_purchase_price<< std::endl;
+
+    double commission_buy = COMMISSION_RATE * stock_purchase_price;
+    std::cout << "Purchase Commission: $" << commission_buy << std::endl;
+
+    double stock_sale_price = SHARES_SOLD * SALE_PRICE;
+    std::cout << "Stock Sale Price: $" << stock_sale_price << std::endl;
+
+    double commission_sale = COMMISSION_RATE * stock_sale_price;
+    std::cout << "Sale Commission: $" << commission_sale << std::endl;
+
+    double profit = stock_sale_price - stock_purchase_price - commission_buy - commission_sale;
+    std::cout << "Total Profit: $" << profit << std::endl;
 
 
 
